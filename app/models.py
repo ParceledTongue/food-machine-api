649340f4640e34f365380c203ede95dd46a3dfb3
ParceledTongue <zach@palumbo.io>
2016-10-28 05:50:52 -0400

    Add Store class to test database
    Signed-off-by: ParceledTongue <zach@palumbo.io>

---
+++ app/models.py
@@ -0,0 +1,7 @@
+class Store(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
+
