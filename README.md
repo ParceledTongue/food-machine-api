4be87603120dfb00d4fee09ab05aa8eb9e669389
ParceledTongue <zach@palumbo.io>
2016-10-29 08:47:07 -0400

    Add README and more views
    Signed-off-by: ParceledTongue <zach@palumbo.io>

---
+++ README.md
@@ -0,0 +1,27 @@
+You will first need [virtualenv](https://pypi.python.org/pypi/virtualenv). 
+Then, in the root project folder, run `virtualenv flask`.
+Once this is done, do `flask/bin'pip install flask.
+
+Then do all of the following (though it is not all necarry now or maybe ever).
+$ flask/bin/pip install flask
+$ flask/bin/pip install flask-login
+$ flask/bin/pip install flask-openid
+$ flask/bin/pip install flask-mail
+$ flask/bin/pip install flask-sqlalchemy
+$ flask/bin/pip install sqlalchemy-migrate
+$ flask/bin/pip install flask-whooshalchemy
+$ flask/bin/pip install flask-wtf
+$ flask/bin/pip install flask-babel
+$ flask/bin/pip install guess_language
+$ flask/bin/pip install flipflop
+$ flask/bin/pip install coverage
+
+Finally, do `./db_migrate` followed by `./db_create` to create the database.
+
+You can see the complete database model in action by interfacing with it
+via the Python shell `/flask/bin/python` and SQLAlchemy calls.
+
+You can access the GET views via your web browser. You can do more complex calls
+with curl, e.g. to add sliced apple: 
+
+curl -i -H "Content-Type: application/json" -X POST -d '{"name":"apple (sliced)", "calories":50,"category":3,"unit":"apple"}' http://localhost:5000/foodmachine/api/ingredients
--- app/views.py
+++ app/views.py
@@ -1,24 +1,60 @@
-from flask import jsonify
-from app import app, models
+from flask import jsonify, request
+from app import app, db, models
 
 pre='/foodmachine/api/'
 
+###############
+# INGREDIENTS #
+###############
+
 @app.route(pre + 'ingredients', methods=['GET'])
 def get_ingredients():
-    # TODO
-    return;
+    ingredients = [ingredient.as_dict() for ingredient in models.Ingredient.query.all()]
+    return jsonify({'ingredients': ingredients})
 
 @app.route(pre + 'ingredients/<int:ingredient_id>', methods=['GET'])
 def get_ingredient(ingredient_id):
     ingredient = models.Ingredient.query.get(ingredient_id).as_dict()
     return jsonify({'ingredient': ingredient})
 
+@app.route(pre + 'ingredients', methods=['POST'])
+def create_ingredient():
+    ingredient = models.Ingredient(
+        name = request.json['name'],
+        calories = request.json['calories'],
+        category = request.json.get('category', 0),
+        unit = request.json.get('unit', 0)
+    )
+    db.session.add(ingredient)
+    db.session.commit()
+    return jsonify({'ingredient': ingredient.as_dict()})
+
+# @app.route(pre + 'ingredients/<int:ingredient_id>', methods=['PUT'])
+# def update_ingredient(ingredient_id):
+# TODO
+
+@app.route(pre + 'ingredients/<int:ingredient_id>', methods=['DELETE'])
+def delete_ingredient(ingredient_id):
+    ingredient = models.Ingredient.query.filter_by(id=ingredient_id);
+    ingredient_dict = ingredient.as_dict()
+    ingredient.delete()
+    db.session.commit()
+    return jsonify({'ingredient': ingredient_dict})
+
+###########
+# RECIPES #
+###########
+
 @app.route(pre + 'recipes', methods=['GET'])
 def get_recipes():
-    # TODO
-    return;
+    recipes = [recipe.as_dict() for recipe in models.Recipe.query.all()]
+    return jsonify({'recipes': recipes})
 
 @app.route(pre + 'recipes/<int:recipe_id>', methods=['GET'])
 def get_recipe(recipe_id):
     recipe = models.Recipe.query.get(recipe_id).as_dict()
     return jsonify({'recipe': recipe})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
