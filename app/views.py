6dd850801e48bfde6f40c93527f4c8211501e75b
ParceledTongue <zach@palumbo.io>
2016-11-14 23:12:13 -0500

    Add 400 error handler
    Signed-off-by: ParceledTongue <zach@palumbo.io>

--- app/views.py
+++ app/views.py
@@ -93,6 +93,10 @@ def get_recipe(recipe_id):
 # OTHER #
 #########
 
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request'}), 404)
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
